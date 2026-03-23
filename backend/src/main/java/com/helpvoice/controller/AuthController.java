75c149098f00464b119c0725fb875c9f48e4085b
viji38068 <viji38068@gmail.com>
2026-03-23 23:12:29 +0530

    final frontend backend connection

--- backend/src/main/java/com/helpvoice/controller/AuthController.java
+++ backend/src/main/java/com/helpvoice/controller/AuthController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*")
 public class AuthController {
 
     @Autowired
